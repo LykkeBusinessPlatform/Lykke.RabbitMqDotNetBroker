f8828d429c2d349738102d2a49b67747b5b01913
atarutin <andrey.tarutin@lykke-business.ch>
2023-06-15 17:39:20 +0300

    introduce NullOutgoingMessagePersister

---
+++ src/Lykke.RabbitMqBroker/Logging/NullOutgoingMessagePersister.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) 2023 Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections.Generic;
+
+namespace Lykke.RabbitMqBroker.Logging
+{
+    internal sealed class NullOutgoingMessagePersister : IOutgoingMessagePersister
+    {
+        public void SetSerializationFormat(SerializationFormat format)
+        {
+        }
+
+        public void Persist<TMessageModel>(byte[] messageBody, IDictionary<string, object> headers)
+        {
+        }
+    }
+}
