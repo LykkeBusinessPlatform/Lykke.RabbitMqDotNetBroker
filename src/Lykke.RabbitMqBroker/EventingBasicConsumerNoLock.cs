037bd1699834c383e694921260d4bb4a46224488
atarutin <andrey.tarutin@lykke-business.ch>
2023-09-19 18:08:52 +0300

    fix(LT-4990): add EventingBasicConsumerNoLock to switch from QueueingConsumer

---
+++ src/Lykke.RabbitMqBroker/EventingBasicConsumerNoLock.cs
@@ -0,0 +1,46 @@
+// Copyright (c) 2023 Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Lykke.RabbitMqBroker
+{
+    public class EventingBasicConsumerNoLock : EventingBasicConsumer
+    {
+        public SharedConcurrentQueue<BasicDeliverEventArgs> Queue { get; private set; }
+        
+        private EventingBasicConsumerNoLock(IModel model) : base(model)
+        {
+        }
+        
+        public EventingBasicConsumerNoLock(IModel model, SharedConcurrentQueue<BasicDeliverEventArgs> queue) : base(model)
+        {
+            Queue = queue;
+            Received += OnReceived;
+            Unregistered += OnUnregistered;
+        }
+
+        private void OnUnregistered(object sender, ConsumerEventArgs e)
+        {
+            Queue.Close();
+        }
+
+        private void OnReceived(object sender, BasicDeliverEventArgs e)
+        {
+            var bodyCopy = new byte[e.Body.Length];
+            Buffer.BlockCopy(e.Body.ToArray(), 0, bodyCopy, 0, e.Body.Length);
+            
+            var eventArgs = new BasicDeliverEventArgs(e.ConsumerTag,
+                e.DeliveryTag,
+                e.Redelivered,
+                e.Exchange,
+                e.RoutingKey,
+                e.BasicProperties,
+                new ReadOnlyMemory<byte>(bodyCopy));
+            
+            Queue.Enqueue(eventArgs);
+        }
+    }
+}
