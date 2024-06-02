fa403078f9da6587553bf686d50d2d8c40a3cab3
atarutin <andrey.tarutin@lykke-business.ch>
2024-06-02 11:38:20 +0300

    chore(LT-5418): clean up code

--- src/Lykke.RabbitMqBroker/Subscriber/MessageReadStrategies/TemplatedMessageReadStrategy.cs
+++ src/Lykke.RabbitMqBroker/Subscriber/MessageReadStrategies/TemplatedMessageReadStrategy.cs
@@ -49,29 +49,4 @@ public abstract class TemplatedMessageReadStrategy : IMessageReadStrategy
             RoutingKey = effectiveRoutingKey
         };
     }
-
-    // public string Configure(RabbitMqSubscriptionSettings settings, IModel channel)
-    // {
-    //     if (settings.ShouldConfigureDeadLetter())
-    //         ConfigureDeadLetter(settings, channel);
-    //         
-    //     var queueName = settings.GetQueueName();
-    //     var args = settings.CreateArguments();
-    //     channel.QueueDeclare(queueName, durable: Durable, exclusive: false, autoDelete: AutoDelete, arguments: args);
-    //
-    //     var effectiveRoutingKey = string.IsNullOrWhiteSpace(_routingKey)
-    //         ? settings.RoutingKey ?? string.Empty
-    //         : _routingKey;
-    //     channel.QueueBind(queueName, settings.ExchangeName, effectiveRoutingKey);
-    //
-    //     return queueName;
-    // }
-    //
-    // private void ConfigureDeadLetter(RabbitMqSubscriptionSettings settings, IModel channel)
-    // {
-    //     var queueName = settings.GetPoisonQueueName();
-    //     channel.ExchangeDeclare(settings.DeadLetterExchangeName, "direct", durable: true);
-    //     channel.QueueDeclare(queueName, durable: Durable, exclusive: false, autoDelete: AutoDelete);
-    //     channel.QueueBind(queueName, settings.DeadLetterExchangeName, settings.RoutingKey ?? string.Empty);
-    // }
 }
