c515dbae0854a5ced0d861e39bb0f604ca83bc88
atarutin <andrey.tarutin@lykke-business.ch>
2024-07-15 13:02:18 +0300

    feat(LT-5418): implement IEnumerable for ConsumerCount

--- tests/Lykke.RabbitMqBroker.Tests/ConsumerCountTests.cs
+++ tests/Lykke.RabbitMqBroker.Tests/ConsumerCountTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 using Lykke.RabbitMqBroker.Subscriber;
 
@@ -103,5 +104,126 @@ public class ConsumerCountTests
         ConsumerCount consumerCount = value;
         string result = consumerCount.ToString();
         Assert.AreEqual(value.ToString(), result);
+    }
+    
+    [Test]
+    public void GetEnumerator_ShouldReturnCorrectSequence()
+    {
+        var consumerCount = new ConsumerCount { Value = 5 };
+        var expectedValues = new[] { 1, 2, 3, 4, 5 };
+        var actualValues = consumerCount.ToList();
+
+        CollectionAssert.AreEqual(expectedValues, actualValues);
+    }
+
+    [TestCase(1, 2)]
+    [TestCase(100, 255)]
+    public void CompareTo_ShouldReturnNegative_WhenFirstIsLessThanSecond(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.Less(count1.CompareTo(count2), 0);
+    }
+
+    [TestCase(2, 1)]
+    [TestCase(255, 100)]
+    public void CompareTo_ShouldReturnPositive_WhenFirstIsGreaterThanSecond(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.Greater(count1.CompareTo(count2), 0);
+    }
+
+    [TestCase(1)]
+    [TestCase(100)]
+    [TestCase(255)]
+    public void CompareTo_ShouldReturnZero_WhenValuesAreEqual(int value)
+    {
+        var count1 = new ConsumerCount { Value = value };
+        var count2 = new ConsumerCount { Value = value };
+        Assert.AreEqual(0, count1.CompareTo(count2));
+    }
+
+    [TestCase(1, 2)]
+    [TestCase(100, 255)]
+    public void LessThanOperator_ShouldReturnTrue_WhenFirstIsLessThanSecond(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.IsTrue(count1 < count2);
+    }
+
+    [TestCase(2, 1)]
+    [TestCase(255, 100)]
+    public void GreaterThanOperator_ShouldReturnTrue_WhenFirstIsGreaterThanSecond(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.IsTrue(count1 > count2);
+    }
+
+    [TestCase(1)]
+    [TestCase(100)]
+    [TestCase(255)]
+    public void Equals_ShouldReturnTrue_WhenValuesAreEqual(int value)
+    {
+        var count1 = new ConsumerCount { Value = value };
+        var count2 = new ConsumerCount { Value = value };
+        Assert.IsTrue(count1.Equals(count2));
+    }
+
+    [TestCase(1, 2)]
+    [TestCase(100, 255)]
+    public void Equals_ShouldReturnFalse_WhenValuesAreNotEqual(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.IsFalse(count1.Equals(count2));
+    }
+
+    [Test]
+    public void GetHashCode_ShouldReturnSameHashCode_ForEqualValues()
+    {
+        var count1 = new ConsumerCount { Value = 100 };
+        var count2 = new ConsumerCount { Value = 100 };
+        Assert.AreEqual(count1.GetHashCode(), count2.GetHashCode());
+    }
+    
+    [TestCase(1, 2)]
+    [TestCase(100, 255)]
+    [TestCase(100, 100)]
+    public void LessThanOrEqualOperator_ShouldReturnTrue_WhenFirstIsLessThanOrEqualToSecond(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.IsTrue(count1 <= count2);
+    }
+
+    [TestCase(2, 1)]
+    [TestCase(255, 100)]
+    public void LessThanOrEqualOperator_ShouldReturnFalse_WhenFirstIsGreaterThanSecond(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.IsFalse(count1 <= count2);
+    }
+
+    [TestCase(2, 1)]
+    [TestCase(255, 100)]
+    [TestCase(100, 100)]
+    public void GreaterThanOrEqualOperator_ShouldReturnTrue_WhenFirstIsGreaterThanOrEqualToSecond(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.IsTrue(count1 >= count2);
+    }
+
+    [TestCase(1, 2)]
+    [TestCase(100, 255)]
+    public void GreaterThanOrEqualOperator_ShouldReturnFalse_WhenFirstIsLessThanSecond(int value1, int value2)
+    {
+        var count1 = new ConsumerCount { Value = value1 };
+        var count2 = new ConsumerCount { Value = value2 };
+        Assert.IsFalse(count1 >= count2);
     }
 }
