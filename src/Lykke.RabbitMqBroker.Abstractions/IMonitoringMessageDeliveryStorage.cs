52ad25f5fd442778735ec5e362d4975ff8a9b549
atarutin <andrey.tarutin@lykke-business.ch>
2024-08-27 17:43:58 +0300

    feat: add message delivery abstractions

---
+++ src/Lykke.RabbitMqBroker.Abstractions/IMonitoringMessageDeliveryStorage.cs
@@ -0,0 +1,8 @@
+namespace Lykke.RabbitMqBroker.Abstractions;
+
+public interface IMonitoringMessageDeliveryStorage
+{
+    Task Add(IMonitoringMessageDelivery messageDelivery);
+    Task<IMonitoringMessageDelivery?> Get(Guid id);
+    Task<IEnumerable<IMonitoringMessageDelivery>> GetByMessageId(Guid messageId);
+}
