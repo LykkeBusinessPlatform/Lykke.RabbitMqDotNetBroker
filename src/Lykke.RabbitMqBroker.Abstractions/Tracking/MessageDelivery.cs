b313845e72a420bd6ab5ab2ac675f8b3b660ad53
atarutin <andrey.tarutin@lykke-business.ch>
2024-09-18 11:26:37 +0300

    test: cover MonitoringHeartbeatReceiver with unit tests

--- src/Lykke.RabbitMqBroker.Abstractions/Tracking/MessageDelivery.cs
+++ src/Lykke.RabbitMqBroker.Abstractions/Tracking/MessageDelivery.cs
@@ -2,7 +2,8 @@ namespace Lykke.RabbitMqBroker.Abstractions.Tracking;
 
 public sealed record MessageDelivery(MessageDeliveryId Id, DateTime? DispatchedTimestamp, DateTime? ReceivedTimestamp, MessageDeliveryFailure Failure)
 {
-    public static MessageDelivery Create() => new(MessageDeliveryId.Create(), null, null, MessageDeliveryFailure.Empty);
+    internal static MessageDelivery Create(MessageDeliveryId id) => new(id, null, null, MessageDeliveryFailure.Empty);
+    public static MessageDelivery Create() => Create(MessageDeliveryId.Create());
     public static readonly MessageDelivery None = new(MessageDeliveryId.Empty, null, null, MessageDeliveryFailure.Empty);
     public bool IsNone => this == None;
 }
