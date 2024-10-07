b49b49a20a262fa5f00351aaac41349daeba36d8
atarutin <andrey.tarutin@lykke-business.ch>
2024-10-07 17:55:49 +0300

    refactor: improve MessageRoute and NonEmptyString ToString() methods

--- src/Lykke.RabbitMqBroker.Abstractions/Tracking/MessageRoute.cs
+++ src/Lykke.RabbitMqBroker.Abstractions/Tracking/MessageRoute.cs
@@ -1,7 +1,18 @@
+using System.Diagnostics;
+
 namespace Lykke.RabbitMqBroker.Abstractions.Tracking;
 
+[DebuggerDisplay("{ToString()}")]
 public record MessageRoute(NonEmptyString ExchangeName, NonEmptyString QueueName, string RoutingKey)
 {
+    public override string ToString()
+    {
+        var baseRouteText = $"[Exchange: {ExchangeName}] -> [Queue: {QueueName}]";
+        return string.IsNullOrWhiteSpace(RoutingKey)
+            ? baseRouteText
+            : $"{baseRouteText} with RoutingKey: {RoutingKey}";
+    }
+
     public static MessageRoute Create(NonEmptyString exchangeName, NonEmptyString queueName, string routingKey) =>
         new(exchangeName, queueName, routingKey);
     public static readonly MessageRoute None = new(new("empty"), new("empty"), string.Empty);
