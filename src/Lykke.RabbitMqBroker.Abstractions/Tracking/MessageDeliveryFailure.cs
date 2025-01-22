ec275d5c9a4e7fdd3e90a27408d0c564bbebb866
atarutin <andrei.tarutin@lykke-business.ch>
2025-01-22 17:41:35 +0300

    fix: use UTC time for message delivery failure timestamps

--- src/Lykke.RabbitMqBroker.Abstractions/Tracking/MessageDeliveryFailure.cs
+++ src/Lykke.RabbitMqBroker.Abstractions/Tracking/MessageDeliveryFailure.cs
@@ -5,7 +5,7 @@ public sealed record MessageDeliveryFailure(MessageDeliveryFailureReason Reason,
     public static readonly MessageDeliveryFailure Empty = new(MessageDeliveryFailureReason.Uncategorised, string.Empty, DateTime.MinValue);
     public bool IsEmpty => this == Empty;
     public static MessageDeliveryFailure Create(MessageDeliveryFailureReason reason, string description = "", DateTime? dateTime = null) =>
-        new(reason, description, dateTime ?? TimeProvider.System.GetLocalNow().DateTime);
+        new(reason, description, dateTime ?? TimeProvider.System.GetUtcNow().DateTime);
     public static MessageDeliveryFailure FromException(Exception exception, MessageDeliveryFailureReason reason = MessageDeliveryFailureReason.Uncategorised, DateTime? dateTime = null) =>
-        new(reason, exception.Message, dateTime ?? TimeProvider.System.GetLocalNow().DateTime);
+        new(reason, exception.Message, dateTime ?? TimeProvider.System.GetUtcNow().DateTime);
 }
