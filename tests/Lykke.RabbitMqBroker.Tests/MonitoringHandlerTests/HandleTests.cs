efd35b67a9a0b6f5d3eb8ac388a19166738da498
atarutin <andrei.tarutin@lykke-business.ch>
2025-01-22 17:37:05 +0300

    refactor: rename ListenerRegistrationHandler to ListenerRegistrationMonitoringHandler

--- tests/Lykke.RabbitMqBroker.Tests/MonitoringHandlerTests/HandleTests.cs
+++ tests/Lykke.RabbitMqBroker.Tests/MonitoringHandlerTests/HandleTests.cs
@@ -1,7 +1,7 @@
 using System.Threading.Tasks;
 
 using Lykke.RabbitMqBroker.Tests.MonitoringMessageSenderTests;
-using MonitoringListenerRegistrationHandler = Lykke.RabbitMqBroker.Monitoring.ListenerRegistrationHandler;
+using MonitoringListenerRegistrationHandler = Lykke.RabbitMqBroker.Monitoring.ListenerRegistrationMonitoringHandler;
 
 using NUnit.Framework;
 using Lykke.RabbitMqBroker.Subscriber;
