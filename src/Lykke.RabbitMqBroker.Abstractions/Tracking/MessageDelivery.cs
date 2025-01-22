9128ad6bb19338bdc51dcb5554f8edf5aa4aa57d
atarutin <andrei.tarutin@lykke-business.ch>
2025-01-22 18:38:03 +0300

    chore(LT-6016): remove internals visibility for unit test projects, no need

--- src/Lykke.RabbitMqBroker.Abstractions/Tracking/MessageDelivery.cs
+++ src/Lykke.RabbitMqBroker.Abstractions/Tracking/MessageDelivery.cs
@@ -2,7 +2,7 @@ namespace Lykke.RabbitMqBroker.Abstractions.Tracking;
 
 public record MessageDelivery(MessageDeliveryId Id, DateTime? DispatchedTimestamp, DateTime? ReceivedTimestamp, MessageDeliveryFailure Failure, MessageRoute Route)
 {
-    internal static MessageDelivery Create(MessageDeliveryId id, MessageRoute route) => new(id, null, null, MessageDeliveryFailure.Empty, route);
+    public static MessageDelivery Create(MessageDeliveryId id, MessageRoute route) => new(id, null, null, MessageDeliveryFailure.Empty, route);
     public static MessageDelivery Create(MessageRoute route) => Create(MessageDeliveryId.Create(), route);
     public static readonly MessageDelivery None = new(MessageDeliveryId.Empty, null, null, MessageDeliveryFailure.Empty, MessageRoute.None);
     public bool IsNone => this == None;
